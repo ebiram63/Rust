ee29941315bcdb941b7dd989db485fb4f3ddb543
ebi63 <ebi.mohamadi63@yahoo.com>
2024-01-24 07:43:21 -0800

    third commit

--- rust-state-machine/src/balances.rs
+++ rust-state-machine/src/balances.rs
@@ -1,6 +1,22 @@
 use std::collections::BTreeMap;
 
 
+let mut map = BTreeMap::new();
+map.insert("alice", 100);
+assert_eq!(map.get(&"alice"), Some(&100));
+assert_eq!(map.get(&"bob"), None);
+
+
+let maybe_value = map.get(&"alice");
+match maybe_value {
+	Some(value) => {
+		// do something with the `value`
+	},
+	None => {
+		// perhaps return an error since there was no value there
+	}
+}
+
 pub struct Pallet {
 	balances: BTreeMap<String, u128>,
 }
